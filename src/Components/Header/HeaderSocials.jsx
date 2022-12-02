20687e1a2f40bd4d67ed5b89ab7a11e2e0577a23
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2022-12-02 20:16:31 +0530

    Social media link updated

--- src/Components/Header/HeaderSocials.jsx
+++ src/Components/Header/HeaderSocials.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import {BsLinkedin} from 'react-icons/bs';
-import {FaGithub} from 'react-icons/fa';
-import {BsTwitter} from 'react-icons/bs'
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-      <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com" target="_blank"><FaGithub/></a>
-      <a href="https://twitter.com" target="_blank"><BsTwitter/></a>
+    <div className="header__socials">
+      <a
+        href="www.linkedin.com/in/kirtiraj-ingale
+"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/kirtirajingale" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://twitter.com/kirtiraj_ingale" target="_blank">
+        <BsTwitter />
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default HeaderSocials;

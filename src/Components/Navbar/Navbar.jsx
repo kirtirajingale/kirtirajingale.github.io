56e8c2147eaea246bc9417d41d8447de89448890
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2023-03-04 23:10:59 +0530

    resume added in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,11 +7,18 @@ import {SiGmail} from "react-icons/si";
 import {FaProjectDiagram} from "react-icons/fa";
 import {BiMessageSquareDetail} from "react-icons/bi";
 import {BiCalendar} from "react-icons/bi";
+import {BsFillCloudDownloadFill} from "react-icons/bs"
 import { useState } from 'react';
 
 
 const Navbar = () => {
   const [activeNav, setActiveNav] = useState("#")
+
+  const handleResume = () =>{
+    window.open(
+      "https://drive.google.com/file/d/1gjSw_aMA140Zr1TdlnI15YsFTYaNhZ3P/view?usp=sharing"
+    )
+  }
   return (
     <nav  id="nav-menu">
         <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === "#" ? 'active' : ""}><AiOutlineHome/></a>
@@ -20,6 +27,7 @@ const Navbar = () => {
         <a href="#projects" onClick={()=> setActiveNav('#projects')} className={activeNav === "#projects" ? 'active' : ""}><FaProjectDiagram/></a>
         <a href="#stats" onClick={()=> setActiveNav('#stats')} className={activeNav === "#stats" ? 'active' : ""}><BiCalendar/></a>
         <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ""}><SiGmail/></a>
+        <a href="#resume" onClick={handleResume} download={"Kirtiraj-Ingale-Resume.pdf"} target="_blank"><BsFillCloudDownloadFill/></a>
        
     </nav>
   )

a204586179088ed9d51b63b6976a9ceb56c81ce3
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2023-04-04 19:05:01 +0530

    updated navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,52 +21,29 @@ const Navbar = () => {
   };
   return (
     <nav id="nav-menu">
-      <a
-        href="#home"
-        onClick={() => setActiveNav("#home")}
-        className="nav-link home"
-      >
+
+      {/* <a href="#home" onClick={() => setActiveNav("#home")} className="nav-link home ">
         <AiOutlineHome />
       </a>
 
-      <a
-        href="#about"
-        onClick={() => setActiveNav("#about")}
-        className="nav-link about"
-      >
+      <a href="#about"  onClick={() => setActiveNav("#about")} className="nav-link about">
         <AiOutlineUser />
       </a>
 
-      <a
-        href="#skills"
-        onClick={() => setActiveNav("#skills")}
-        className="nav-link skills"
-      >
-        <RiMedalFill />
+      <a href="#skills" onClick={() => setActiveNav("#skills")} className="nav-link skills">
+        <RiMedalFill/>
       </a>
 
-      <a
-        href="#projects"
-        onClick={() => setActiveNav("#projects")}
-        className="nav-link projects"
-      >
+      <a  href="#projects"  onClick={() => setActiveNav("#projects")}  className="nav-link projects">
         <FaProjectDiagram />
       </a>
 
-      <a
-        href="#stats"
-        onClick={() => setActiveNav("#stats")}
-        className={activeNav === "#stats" ? "active" : ""}
-      >
-        <BiCalendar />
+      <a  href="#stats"  onClick={() => setActiveNav("#stats")}  className="nav-link stats">
+        <BiCalendar/>
       </a>
 
-      <a
-        href="#contact"
-        onClick={() => setActiveNav("#contact")}
-        className="nav-link contact"
-      >
-        <SiGmail />
+      <a  href="#contact" onClick={() => setActiveNav("#contact")}  className="nav-link contact">
+       <SiGmail/>
       </a>
 
       <button id="resume-button-1" onClick={handleResume}>
@@ -75,14 +52,97 @@ const Navbar = () => {
           href={CV}
           download="Kirtiraj-Ingale-Resume"
           target="_blank"
-          className="nav-link resume"
+          className="nav-link resume icon"
           rel="noreferrer"
         >
-          <BsFillCloudDownloadFill />
+          <div className="tooltip">Resume</div>
+          <BsFillCloudDownloadFill/>
+        </a>
+      </button> */}
+
+
+      <div className="icon">
+        <div className="tooltip">Home</div>
+        <span>
+        <a href="#home" onClick={() => setActiveNav("#home")} className="nav-link home ">
+        <AiOutlineHome className="nav-icons" />
+        </a>
+        </span>
+      </div>
+
+      <div className="icon">
+        <div className="tooltip">About</div>
+        <span>
+        <a href="#about"  onClick={() => setActiveNav("#about")} className="nav-link about">
+        <AiOutlineUser className="nav-icons" />
+        </a>
+        </span>
+      </div>
+
+      <div className="icon stats-icon">
+        <div className="tooltip">Skills</div>
+        <span>
+        <a href="#skills" onClick={() => setActiveNav("#skills")} className="nav-link skills">
+        <RiMedalFill className="nav-icons"/>
+        </a>
+        </span>
+      </div>
+
+      <div className="icon">
+        <div className="tooltip">Projects</div>
+        <span>
+        <a  href="#projects"  onClick={() => setActiveNav("#projects")}  className="nav-link projects">
+        <FaProjectDiagram className="nav-icons"/>
+        </a>
+        </span>
+      </div>
+
+      <div className="icon stats-icon">
+        <div className="tooltip">Stats</div>
+        <span>
+        <a  href="#stats"  onClick={() => setActiveNav("#stats")}  className="nav-link stats">
+        <BiCalendar className="nav-icons"/>
+        </a>
+        </span>
+      </div>
+
+      <div className="icon">
+        <div className="tooltip">Contact</div>
+        <span>
+        <a  href="#contact" onClick={() => setActiveNav("#contact")}  className="nav-link contact">
+        <SiGmail className="nav-icons"/>
+        </a>
+        </span>
+      </div>
+
+      <div className="icon">
+        <div className="tooltip">Resume</div>
+        <span>
+        <button id="resume-button-1" onClick={handleResume}>
+        <a
+          id="resume-link-1"
+          href={CV}
+          download="Kirtiraj-Ingale-Resume"
+          target="_blank"
+          className="nav-link resume icon"
+          rel="noreferrer"
+        >
+          <div className="tooltip">Resume</div>
+          <BsFillCloudDownloadFill className="nav-icons"/>
         </a>
       </button>
+        </span>
+      </div>
+
+
+
+
+
+
     </nav>
   );
 };
 
 export default Navbar;
+
+
